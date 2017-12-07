bde85b7f6fff8af1480a562e635543a85b4e23a3
andzav <andzav@users.noreply.github.com>
2017-12-07 13:50:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -68,11 +68,13 @@ app.get('/:filename', function(req, res) {
     userModel.findOne({'sessions.ip': ip, 'sessions.fingerprint': req.fingerprint.hash}, 'permission' , function (err, person) {
         if (err) res.sendStatus(400);
         else if(person){
+            console.log(person)
             if(person.permission==='admin') res.sendFile(path.join(__dirname, 'public', 'pages', req.params.filename));
             else if(person.permission==='operator'){
                 if(req.params.filename==='users') res.sendFile(path.join(__dirname, 'public', 'pages', '404.html'));
                 else res.sendFile(path.join(__dirname, 'public', 'pages', req.params.filename));
             }else{
+                console.log(req.params.filename);
                 if(req.params.filename!=='main_user_page.html'&&req.params.filename!=='log-in.html'&&req.params.filename!=='sing-up.html'&&req.params.filename!=='index.html')                  res.sendFile(path.join(__dirname, 'public', 'pages', '404.html'));
                 else res.sendFile(path.join(__dirname, 'public', 'pages', req.params.filename));
             }
